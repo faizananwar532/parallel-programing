ff2ef2a7f6434e2109e01bdc481f56140cd5f0ae
fd0002007 <fd0002007@manager.hpc.informatik.hs-fulda.de>
2025-11-25 12:06:11 +0100

    modified ex4

--- ex4/slurmSubmit.sh
+++ ex4/slurmSubmit.sh
@@ -13,9 +13,9 @@
 #SBATCH --nodes=1
 
 ####### Output #######
-SBATCH --output=/home/fd0002007/out/matmul.out.%j
-SBATCH --error=/home/fd0002007/out/matmul.err.%j
+#SBATCH --output=/home/fd0002007/out/matmul.out.%j
+#SBATCH --error=/home/fd0002007/out/matmul.err.%j
 
 export OMP_NUM_THREADS=4
-#cd /path/to/bin
+cd /home/fd0002007/parallel-programing/ex4
 ./matmul
